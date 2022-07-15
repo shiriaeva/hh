2d92eef0d22d7198712791eb4286e72e34a173b9
shiriaeva <84004210+shiriaeva@users.noreply.github.com>
2022-07-15 16:05:49 +0500

    изменение бд

--- headhunter_app/main.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import requests
-from bs4 import BeautifulSoup
-from sqlalchemy.orm import Session
-from transfer_to_bd import *
-from hh_parser import get_jobs
-
-get_jobs(".net+framework")
-
